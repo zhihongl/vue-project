972ecd252bee095b6aec49d58f10c2fc41ed71d4
Jake Lu <zhihonglu2000@gmail.com>
2025-04-22 22:03:18 +1000

    chore: initialize project with Vue 3, Vite, and essential configurations
    This commit sets up a new Vue 3 project using Vite, including basic configurations for TypeScript, ESLint, Prettier, Vitest, and Playwright. It also includes initial components, views, and assets necessary for a standard Vue application.

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const s1 = document.createElement('script')
+  s1.async = true
+  s1.src = 'https://embed.tawk.to/6807825a8e49a91911cf42f3/1ipelad1r'
+  s1.charset = 'UTF-8'
+  s1.setAttribute('crossorigin', '*')
+  const s0 = document.getElementsByTagName('script')[0]
+  s0.parentNode.insertBefore(s1, s0)
+})
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
